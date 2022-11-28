05dd9c3508501c73bf97eec35c2425addce7b19b
josecaos <jazzvoon@gmail.com>
2022-11-28 01:08:55 -0600

    Comienza migracion de version legacy

---
+++ src/components/Background.svelte
@@ -0,0 +1,19 @@
+<script>
+    
+    export let image = "../images/1.png";
+
+</script>
+
+<section id="background-general">
+
+
+    <div class="back">
+
+        <img src="{image}" alt="Mixfuckedup">
+
+
+    </div>
+
+
+
+</section>
