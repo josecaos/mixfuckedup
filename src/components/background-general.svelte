3b7684792538c7136ea6e28dc0db63d79e70dea0
josecaos <jazzvoon@gmail.com>
2022-11-28 00:42:23 -0600

    Primeros archivos

---
+++ src/components/background-general.svelte
@@ -0,0 +1,22 @@
+<script>
+    // import //algo  
+    
+    export image '../images/background-general';
+</script>
+
+
+
+
+<section id="background-general">
+
+
+    <div class="back">
+
+        <img src="{image}" alt="">
+
+
+    </div>
+
+
+
+</section>
