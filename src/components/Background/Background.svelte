a1178d4613837537f0fc042526e7a8bb7dab410d
josecaos <jazzvoon@gmail.com>
2022-11-30 12:16:03 -0600

    Debugging Papperjs

--- src/components/Background/Background.svelte
+++ src/components/Background/Background.svelte
@@ -1,6 +1,7 @@
 <script>
-	import { onMount } from 'svelte'; 
-	import paper from 'paper';
+	// import { Path } from 'path';
+	import { paper, Raster, View } from 'paper';
+	import { onMount} from 'svelte';
 	import randomBackground from './Background.js';
 	import debugImage1 from '../../assets/images/1.png';
 	import debugImage2 from '../../assets/images/2.png';
@@ -28,80 +29,75 @@
 	//
 	let element;
 
-
 	onMount(() => {
+		//TODO: seguir debuggeando el objeto paper y checar si por ahi se puede lograr algo
+		console.log(paper);
+		// 	// Create a raster item:
+		// let raster = Raster();
+		// let raster = new Raster({
+		// 	source: element.src,
+		// 	// position: 'center'
+		// });
+		// let loaded = false;
+		// 	//
 
-		console.log(element, "???");
+		// raster.onLoad = function () {
+		// 	loaded = true;
+		// 	// onResize();
+		// };
+		// 	// Make the raster invisible:
+		// 	raster.visible = true; //default: false
+		// 	// let lastPos = view.center;
 
-		// 
-	let raster = new Raster(element);
-	let loaded = false;
-	
+		// 	function moveHandler(event) {
+		// 		if (!loaded) return;
+
+		// 		if (lastPos.getDistance(event.point) < 1) return;
+		// 		lastPos = event.point;
+
+		// 		let size = this.bounds.size.clone();
+		// 		let isLandscape = size.width > size.height;
+
+		// 		// If the path is in landscape orientation, we're going to
+		// 		// split the path horizontally, otherwise vertically:
+
+		// 		size /= isLandscape ? [2, 1] : [1, 2];
+
+		// 		let vector;
+		// 		vector = new Path.Rectangle({
+		// 			point: this.bounds.topLeft.floor(),
+		// 			size: size.ceil(),
+		// 			onMouseMove: moveHandler
+		// 		});
+		// 		vector.fillColor = raster.getAverageColor(vector);
+
+		// 		vector = new Path.Rectangle({
+		// 			point: isLandscape ? this.bounds.topCenter.ceil() : this.bounds.leftCenter.ceil(),
+		// 			size: size.floor(),
+		// 			onMouseMove: moveHandler
+		// 		});
+		// 		vector.fillColor = raster.getAverageColor(vector);
+
+		// 		this.remove();
+		// 	}
+		// 	//
+		// 	function onResize(event) {
+		// 		if (!loaded) return;
+		// 		project.activeLayer.removeChildren();
+
+		// 		// Transform the raster so that it fills the bounding rectangle
+		// 		// of the view:
+		// 		raster.fitBounds(view.bounds, true);
+
+		// 		// Create a path that fills the view, and fill it with
+		// 		// the average color of the raster:
+		// 		new Path.Rectangle({
+		// 			rectangle: view.bounds,
+		// 			fillColor: raster.getAverageColor(view.bounds),
+		// 			onMouseMove: moveHandler
+		// 		});
+		// 	}
 	});
-
-
-
-
-
-	// // Create a raster item:
-	// let raster = new Raster(element);
-	// let loaded = false;
-
-	// raster.on('load', function () {
-	// 	loaded = true;
-	// 	onResize();
-	// });
-
-	// // Make the raster invisible:
-	// raster.visible = true; //default: false
-
-	// let lastPos = view.center;
-	// function moveHandler(event) {
-	// 	if (!loaded) return;
-	// 	if (lastPos.getDistance(event.point) < 1) return;
-	// 	lastPos = event.point;
-
-	// 	let size = this.bounds.size.clone();
-	// 	let isLandscape = size.width > size.height;
-
-	// 	// If the path is in landscape orientation, we're going to
-	// 	// split the path horizontally, otherwise vertically:
-
-	// 	size /= isLandscape ? [2, 1] : [1, 2];
-
-	// 	let path = new Path.Rectangle({
-	// 		point: this.bounds.topLeft.floor(),
-	// 		size: size.ceil(),
-	// 		onMouseMove: moveHandler
-	// 	});
-	// 	path.fillColor = raster.getAverageColor(path);
-
-	// 	let path = new Path.Rectangle({
-	// 		point: isLandscape ? this.bounds.topCenter.ceil() : this.bounds.leftCenter.ceil(),
-	// 		size: size.floor(),
-	// 		onMouseMove: moveHandler
-	// 	});
-	// 	path.fillColor = raster.getAverageColor(path);
-
-	// 	this.remove();
-	// }
-
-	// function onResize(event) {
-	// 	if (!loaded) return;
-	// 	project.activeLayer.removeChildren();
-
-	// 	// Transform the raster so that it fills the bounding rectangle
-	// 	// of the view:
-	// 	raster.fitBounds(view.bounds, true);
-
-	// 	// Create a path that fills the view, and fill it with
-	// 	// the average color of the raster:
-	// 	new Path.Rectangle({
-	// 		rectangle: view.bounds,
-	// 		fillColor: raster.getAverageColor(view.bounds),
-	// 		onMouseMove: moveHandler
-	// 	});
-	// }
 </script>
 
 <section id="background">
