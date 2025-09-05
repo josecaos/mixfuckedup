cacb3bed54cec06ba4cf86e0dd26cd827f300d3e
josecaos <jazzvoon@gmail.com>
2025-09-05 17:52:10 -0600

    Crea template para release Hackpact

---
+++ music/hackpactMX_jul2011/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>josecaos - HackpactMX julio 2011</title>
+</head>
+<body>
+    <h1>josecaos - HackpactMX julio 2011</h1>
+    <ul>
+        <li><a href="03jul11-JCHasbun.mp3" target="_blank">03jul11-JCHasbun.mp3</a></li>
+        <li><a href="04jul11-JCHasbun.mp3" target="_blank">04jul11-JCHasbun.mp3</a></li>
+        <li><a href="05jul11-JCHasbun.mp3" target="_blank">05jul11-JCHasbun.mp3</a></li>
+        <li><a href="06jul11-JCHasbun.mp3" target="_blank">06jul11-JCHasbun.mp3</a></li>
+        <li><a href="07jul11-JCHasbun.mp3" target="_blank">07jul11-JCHasbun.mp3</a></li>
+        <li><a href="08jul11-JCHasbun.mp3" target="_blank">08jul11-JCHasbun.mp3</a></li>
+        <li><a href="10jul11-JCHasbun.mp3" target="_blank">10jul11-JCHasbun.mp3</a></li>
+        <li><a href="11jul11-JCHasbun.mp3" target="_blank">11jul11-JCHasbun.mp3</a></li>
+        <li><a href="13jul11-JCHasbun.mp3" target="_blank">13jul11-JCHasbun.mp3</a></li>
+        <li><a href="18jul11-JCHasbun.mp3" target="_blank">18jul11-JCHasbun.mp3</a></li>
+        <li><a href="20jul11-JCHasbun.mp3" target="_blank">20jul11-JCHasbun.mp3</a></li>
+        <li><a href="21jul11-JCHasbun.mp3" target="_blank">21jul11-JCHasbun.mp3</a></li>
+        <li><a href="22jul11-JCHasbun.mp3" target="_blank">22jul11-JCHasbun.mp3</a></li>
+        <li><a href="27jul11-JCHasbun.mp3" target="_blank">27jul11-JCHasbun.mp3</a></li>
+    </ul>
+</body>
+</html>
