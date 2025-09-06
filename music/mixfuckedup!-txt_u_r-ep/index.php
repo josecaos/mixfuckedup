5cf7d0de152c915f4989c4ced4a75ecebf42626d
josecaos <jazzvoon@gmail.com>
2025-09-05 18:06:38 -0600

    Agrega template para TxT_U_R EP

---
+++ music/mixfuckedup!-txt_u_r-ep/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>josecaos - TXT_U_R EP</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+        ul {
+            list-style: none;
+        }
+        ul li {
+            margin: 0.25rem 0;
+        }
+        a {
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <h1>josecaos - TXT_U_R EP</h1>
+    <div style="display:flex; justify-content: center;">
+        <input type="image" src="portada_txtur_ep.jpg" alt="josecaos | TXT_U_R EP">
+    </div>
+    <ul style="display:flex; flex-direction: column; align-items: center;">
+        <li><a href="cuartos_de_metal.mp3" target="_blank">cuartos_de_metal.mp3</a></li>
+        <li><a href="mascando_piedras.mp3" target="_blank">mascando_piedras.mp3</a></li>
+        <li><a href="mascando_mas_piedras.mp3" target="_blank">mascando_mas_piedras.mp3</a></li>
+    </ul>
+</body>
+</html>
